c62b3c1d0cac4c4ee36b4fc5c341f7eb6beeaa7d
Erick Gabriel <erickgabrielalves@hotmail.com>
2025-04-28 23:32:51 -0300

    feat: ajuste teste

--- cypress/e2e/busca.cy.js
+++ cypress/e2e/busca.cy.js
@@ -6,31 +6,28 @@ describe('US-12 : Funcionalidade: busca de filmes', () => {
     });
 
     afterEach(() => {
-        Cypress.Screenshot()
+        cy.screenshot();
     });
 
-    it('Deve buscar filme com sucesso', () =>{
-    cy.get('#search-input').type('matrix')
-    cy.get('#search-button').click()
-    cy.get('#results-section').should('contain' , 'Matrix')
+    it('Deve buscar filme com sucesso', () => {
+        cy.get('#search-input').type('matrix')
+        cy.get('#search-button').click()
+        cy.get('#results-section').should('contain', 'Matrix')
     });
 
-    it('Deve Buscar filmes com sucesso de uma lista', () =>{
-        cy.fixture('filmes').then((filmes) =>{
+    it('Deve Buscar filmes com sucesso de uma lista', () => {
+        cy.fixture('filmes').then((filmes) => {
             cy.get('#search-input').type(filmes[0].titulo)
             cy.get('#search-button').click()
-            cy.get('#results-section').should('contain' , filmes[0].titulo)
+            cy.get('#results-section').should('contain', filmes[0].titulo)
         })
     });
 
     it.only('Deve Buscar filmes com sucesso da lista inteiro', () => {
         cy.fixture('filmes').each((filmes) => {
             cy.get('#search-input').clear().type(filmes.titulo)
-            cy.get('#search-button').click({force: true})
-            cy.get('#results-section').should('contain' , filmes.titulo)
+            cy.get('#search-button').click({ force: true })
+            cy.get('#results-section').should('contain', filmes.titulo)
         })
     });
-
 });
-
-
--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -18,35 +18,35 @@ describe('US-012 Funcionalidade: Cadastro de membros', () => {
   });
 
   //it('CT-03 - Validar mensagem de erro com email inválido', () => {
-    //cy.preencherCadastro('Leonardo', 'Baldo', 'email-invalido', '123456', 'Teste@123456');
-    //cy.get('#signup-response').should('contain', 'Por favor, insira um email válido');
+  //cy.preencherCadastro('Leonardo', 'Baldo', 'email-invalido', '123456', 'Teste@123456');
+  //cy.get('#signup-response').should('contain', 'Por favor, insira um email válido');
   //});
 
   //it('CT-04 - Validar mensagem de erro com senha fraca', () => {
-    //const email = `teste${Date.now()}@invalido.com`;
+  //const email = `teste${Date.now()}@invalido.com`;
 
-    //cy.preencherCadastro('Leonardo', 'Baldo', email, '123456', 'SenhaFraca123');
-    //cy.get('#signup-response').should('contain', 'A senha deve conter pelo menos um caractere especial');
-    
-   
-    //cy.preencherCadastro('Leonardo', 'Baldo', email, '123456', 'Abc1@');
-    //cy.get('#signup-response').should('contain', 'A senha deve ter no mínimo 8 caracteres');
+  //cy.preencherCadastro('Leonardo', 'Baldo', email, '123456', 'SenhaFraca123');
+  //cy.get('#signup-response').should('contain', 'A senha deve conter pelo menos um caractere especial');
+
+
+  //cy.preencherCadastro('Leonardo', 'Baldo', email, '123456', 'Abc1@');
+  //cy.get('#signup-response').should('contain', 'A senha deve ter no mínimo 8 caracteres');
   //});
 
   //it('CT-05 - Validar mensagem de erro quando senhas não coincidem', () => {
-    //const email = `teste${Date.now()}@invalido.com`;
-    //cy.preencherCadastro('Leonardo', 'Baldo', email, '123456', 'Teste@123456', 'Teste@654321');
-    //cy.get('#signup-response').should('contain', 'As senhas não coincidem');
+  //const email = `teste${Date.now()}@invalido.com`;
+  //cy.preencherCadastro('Leonardo', 'Baldo', email, '123456', 'Teste@123456', 'Teste@654321');
+  //cy.get('#signup-response').should('contain', 'As senhas não coincidem');
   //});
 
   //it('CT-06 - Validar mensagem de erro com campo telefone inválido', () => {
-    //const email = `teste${Date.now()}@invalido.com`;
-    //cy.preencherCadastro('Leonardo', 'Baldo', email, 'abc123', 'Teste@123456');
-    //cy.get('#signup-response').should('contain', 'O telefone deve conter apenas números');
+  //const email = `teste${Date.now()}@invalido.com`;
+  //cy.preencherCadastro('Leonardo', 'Baldo', email, 'abc123', 'Teste@123456');
+  //cy.get('#signup-response').should('contain', 'O telefone deve conter apenas números');
   //});
-//});
+  //});
 
-//Cypress.Commands.add('preencherCadastro', (nome, sobrenome, email, telefone, senha, confirmarSenha = senha) => {
+  //Cypress.Commands.add('preencherCadastro', (nome, sobrenome, email, telefone, senha, confirmarSenha = senha) => {
   //cy.get('#firstName').type(nome);
   //cy.get('#lastName').type(sobrenome);
   //cy.get('#email').type(email);
